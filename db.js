5a19739d460703189ec785f6a361fb756582528a
SameerKhan <sameerkhanyt09@gmail.com>
2024-06-16 00:41:52 +0530

    useNewUrlParser

--- db.js
+++ db.js
@@ -3,7 +3,13 @@ require("dotenv").config();
 
 const MongoURL = process.env.MONGODB_URL;
 
-mongoose.connect(MongoURL);
+mongoose.connect(
+  "mongodb+srv://sameerKhan01:samSAM@cluster0.lgodr8g.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
